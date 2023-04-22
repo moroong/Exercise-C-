616ebf80b12d6fd318d2a8cb6da6ef5adaa146ed
moroong <122975190+moroong@users.noreply.github.com>
2023-04-22 14:19:38 +0900

    Add files via upload

--- Example02-Loop.c
+++ Example02-Loop.c
@@ -1,28 +1,50 @@
 /*
 	File Name : Example02-Loop.c
-	Data	  : 2023. 03. 11.
+	Data	  : 2023. 03. 12.
 */
 
 #include <stdio.h>
 
 int main()
 {
-	// 사용자로부터 두 개의 정수를 입력을 받고
-	// 두 정수를 포함한 두 정수 사이의 모든 값들을 출력하는 프로그램을 작성
-	//  - 오름차순으로 출력
+	// 소수(Prime number)
+	//  : 약수가 1과 자기 자신뿐인 1보다 큰 자연수
+	//    → 2, 3, 5, 7, 11 ...
+	//  : 1과 자기 자신을 제외한 다른 자연수로 나누어 떨어지면
+	//	  소수가 아니다.
+	
+	// 소수인지 판별하는 프로그램을 작성
+	//  - 사용자가 임의로 입력한 2 이상의 자연수를 입력
+	//  - 사용자가 입력한 자여수가 소수인지 아닌지 출력
+	
+	// 플래그 변수(flag variable)
+	//  - 특정 상태를 나타내기 위한 변수
+	int is_prime = 1;  // 소수인지 아닌지 판별하기 위한 변수
+	// 1 : 소수인 경우
+	// 0 : 소수가 아닌 경우
 
-	int n1, n2;
-	int start, end;
-	printf("Enter two integer>>>");
-	scanf("%d %d", &n1, &n2);
+	int num = 0;
 
-	start = n1 < n2 ? n1 : n2;
-	end = n1 > n2 ? n1 : n2;
+	printf("Enter a positive number greater than 1 >>>");
+	scanf("%d", &num);
+	printf("\n");
 
-	for (int n = start; n <= end; n++) {
-		printf("n = %d\n", n);
+	// 입력 수의 1과 자기 자신 사이의 수들을 대상으로
+	// 나누어 떨어지는지 검사
+	//  → 약수를 찾기 위해 2부터 num의 제곱근까지의 자연수들만 검사
+	for (int n = 2; n * n <= num; n++)
+	{
+		if (num % n == 0)
+		{
+			is_prime = 0;
+			break;
+		}
 	}
 
-
+	if (is_prime)  // == 1
+		printf("%d is a prime number\n", num);
+	else		   // == 0
+		printf("%d is not a prime number\n", num);
+	
 	return 0;
 }
